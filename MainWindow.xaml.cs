959ccc2ce3110580feb6a4ed6e1cb4f3606398c0
TT432 <2437315224@qq.com>
2022-05-27 21:45:55 +0800

    图片显示 ok

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ScreenCreatorMC.ResourcesManager;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,63 @@ namespace ScreenCreatorMC
         public MainWindow()
         {
             InitializeComponent();
+
+            ResourceDataList.Instance.Init();
+
+            ResourceDataListBox.ItemsSource = ResourceDataList.Instance;
+        }
+
+        private void Grid_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        public static readonly string imagePrefix = "images/";
+
+        private void Grid_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                (from data in (IList<string>)e.Data.GetData(DataFormats.FileDrop)
+                where data != null && data.ToLower().EndsWith(".png")
+                select data).ToList().ForEach(image =>
+                {
+                    FileInfo imageInfo = new(image);
+
+                    CopyFile(image, imagePrefix + imageInfo.Name);
+                });
+
+                ResourceDataList.Instance.ReloadResources();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void CopyFile(string o, string t)
+        {
+            FileInfo file1 = new(o);
+            FileInfo file2 = new(t);
+
+            if (file2.Exists)
+            {
+                file2.Delete();
+            }
+
+            file1.CopyTo(t);
+        }
+
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
+        {
+
         }
     }
 }
---
+++ ResourcesManager/ResourceData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Collections;
+using System.ComponentModel;
+
+namespace ScreenCreatorMC.ResourcesManager
+{
+    public class ResourceDataList : ObservableCollection<ResourceData>, IEnumerable<ResourceData>
+    {
+        private static readonly ResourceDataList _instance = new();
+        public static ResourceDataList Instance { get { return _instance; } }
+
+        private ResourceDataList()
+        {
+
+        }
+
+        public static readonly DirectoryInfo images = new("./images");
+
+        public void Init()
+        {
+            if (!images.Exists)
+            {
+                images.Create();
+            }
+
+            ReloadResources();
+        }
+
+        public void ReloadResources()
+        {
+            Clear();
+
+            images.GetFiles().ToList().ForEach(file => {
+                if (file.Name.ToLower().EndsWith(".png"))
+                {
+                    Add(new ResourceData(file.Name, new(0, 0)));
+                }
+            });
+        }
+    }
+
+    /**
+     * 图片的数据，只接受 png 格式
+     */
+    public class ResourceData
+    {
+        public string ResourceName { get; set; }
+        public string ResourcePath
+        {
+            get
+            {
+                return "pack://SiteOfOrigin:,,,/images/" + ResourceName;
+            }
+        }
+
+        public Point Size { get; set; }
+
+        public ResourceData(string resourceName, Point size)
+        {
+            ResourceName = resourceName;
+            Size = size;
+        }
+    }
+}
